01ca72b72696eac290c1b0e7f53ac7a790e4636c
Frank <531915558@qq.com>
2020-07-09 23:02:08 +0800

    init

---
+++ src/main/java/com/example/concurrencytutorials/ConcurrencyTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrencytutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyTutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concurrencytutorials/ConcurrencyTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrencytutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
